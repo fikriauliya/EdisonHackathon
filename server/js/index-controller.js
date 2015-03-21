e3dfbf963d59664644bca5fa8ab4d5f50b771618
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2015-03-21 16:21:06 +0800

    Refactor

--- server/js/index-controller.js
+++ server/js/index-controller.js
@@ -5,135 +5,70 @@ app.controller("IndexCtrl", function($scope, $http, $q, $filter, $location) {
   var dateNow = moment().format("YYYY-MM-DD");
   var timeNow = moment().format("HH:mm:ss");
 
-  $scope.curTemperature = 0;
-  $scope.curNoise = 0;
-  $scope.curLight = 0;
+  $scope.curtemperature = 0;
+  $scope.curnoise = 0;
+  $scope.curlight = 0;
 
   var updateChart = function() {
     var curDate;
     var i = 0;
     curDate = moment().subtract(i, 'days').format("YYYY-MM-DD");
-    console.log(curDate);
+    
+    var criteria = ["temperature", "noise", "light"];
+    for (var j=0;j<criteria.length;j++) {
+      (function(c) {
+        ref.child(0).child(c).child(curDate).on("value", function(snapshot) {
+          var history = snapshot.val();
+    
+          var labels = [];
+          var data = [];
 
-    ref.child(0).child("temperature").child(curDate).on("value", function(snapshot) {
-      var history = snapshot.val();
-      console.log(history);
+          var lastHistory = 0;
 
-      var labels = [];
-      var data = [];
+          var historyLength = 0;
+          for (var k in history) {
+            if (history.hasOwnProperty(k)) {
+              data.push(history[k]);
+              lastHistory = history[k];
 
-      var lastHistory = 0;
-      for (var k in history) {
-        if (history.hasOwnProperty(k)) {
-          labels.push(k);
-          data.push(history[k]);
+              historyLength++;
+            }
+          }
 
-          lastHistory = history[k];
-        }
-      }
+          var i = 0;
+          for (var k in history) {
+            if (i % Math.floor(historyLength / 20) == 0) {
+              labels.push(k);  
+            } else {
+              labels.push("");
+            }
+            i++;
+          }
 
-      $scope.$apply(function() {
-        $scope.curTemperature = lastHistory;
-      })
+          $scope.$apply(function() {
+            $scope["cur" + c] = lastHistory;
+          })
 
-      var ctx = document.getElementById("temperatureChart").getContext("2d");
-      var chartData = {
-          labels: labels,
-          datasets: [
-              {
-                  label: "My First dataset",
-                  fillColor: "rgba(220,220,220,0.2)",
-                  strokeColor: "rgba(220,220,220,1)",
-                  pointColor: "rgba(220,220,220,1)",
-                  pointStrokeColor: "#fff",
-                  pointHighlightFill: "#fff",
-                  pointHighlightStroke: "rgba(220,220,220,1)",
-                  data: data
-              }
-          ]
-      };
-      var myNewChart = new Chart(ctx).Line(chartData, {animation: false});
-    });
-
-    ref.child(0).child("noise").child(curDate).on("value", function(snapshot) {
-      var history = snapshot.val();
-      console.log(history);
-
-      var labels = [];
-      var data = [];
-
-      var lastHistory = 0;
-      for (var k in history) {
-        if (history.hasOwnProperty(k)) {
-          labels.push(k);
-          data.push(history[k]);
-
-          lastHistory = history[k];
-        }
-      }
-
-      $scope.$apply(function() {
-        $scope.curNoise = lastHistory;
-      })
-
-      var ctx = document.getElementById("noiseChart").getContext("2d");
-      var chartData = {
-          labels: labels,
-          datasets: [
-              {
-                  label: "My First dataset",
-                  fillColor: "rgba(220,220,220,0.2)",
-                  strokeColor: "rgba(220,220,220,1)",
-                  pointColor: "rgba(220,220,220,1)",
-                  pointStrokeColor: "#fff",
-                  pointHighlightFill: "#fff",
-                  pointHighlightStroke: "rgba(220,220,220,1)",
-                  data: data
-              }
-          ]
-      };
-      var myNewChart = new Chart(ctx).Line(chartData, {animation: false});
-    });
-
-    ref.child(0).child("light").child(curDate).on("value", function(snapshot) {
-      var history = snapshot.val();
-      console.log(history);
-
-      var labels = [];
-      var data = [];
-
-      var lastHistory = 0;
-      for (var k in history) {
-        if (history.hasOwnProperty(k)) {
-          labels.push(k);
-          data.push(history[k]);
-
-          lastHistory = history[k];
-        }
-      }
-
-      $scope.$apply(function() {
-        $scope.curLight = lastHistory;
-      })
-
-      var ctx = document.getElementById("lightChart").getContext("2d");
-      var chartData = {
-          labels: labels,
-          datasets: [
-              {
-                  label: "My First dataset",
-                  fillColor: "rgba(220,220,220,0.2)",
-                  strokeColor: "rgba(220,220,220,1)",
-                  pointColor: "rgba(220,220,220,1)",
-                  pointStrokeColor: "#fff",
-                  pointHighlightFill: "#fff",
-                  pointHighlightStroke: "rgba(220,220,220,1)",
-                  data: data
-              }
-          ]
-      };
-      var myNewChart = new Chart(ctx).Line(chartData, {animation: false});
-    });
+          var ctx = document.getElementById(c + "Chart").getContext("2d");
+          var chartData = {
+              labels: labels,
+              datasets: [
+                  {
+                      label: "My First dataset",
+                      fillColor: "rgba(220,220,220,0.2)",
+                      strokeColor: "rgba(220,220,220,1)",
+                      pointColor: "rgba(220,220,220,1)",
+                      pointStrokeColor: "#fff",
+                      pointHighlightFill: "#fff",
+                      pointHighlightStroke: "rgba(220,220,220,1)",
+                      data: data
+                  }
+              ]
+          };
+          var myNewChart = new Chart(ctx).Line(chartData, {animation: false, scaleShowVerticalLines: false});
+        });
+      })(criteria[j]);
+    }
   }
 
   updateChart();
